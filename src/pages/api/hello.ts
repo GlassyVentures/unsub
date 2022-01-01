1e3d117fcd01316ff2c51ca2ca370ed32c472ca3
Christian Glassiognon <christian@heyglassy.co>
2022-01-01 01:26:25 -0700

    Testing

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    await browser.close();
+  })();
+
+  res.status(200).json({ name: "John Doe" });
 }

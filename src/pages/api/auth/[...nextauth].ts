02c9cd80188150ce6546dcc9df6fa64e4e8321a7
Christian Glassiognon <christian@heyglassy.co>
2022-01-04 22:02:50 -0700

    Updated flow for early access users that have not been onboarded

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -30,6 +30,31 @@ export default NextAuth({
       return baseUrl;
     },
     async session({ user, session }) {
+      const onboard = await prisma.onboard.findUnique({
+        where: {
+          email: user.email!,
+        },
+      });
+
+      if (onboard) {
+        await prisma.user.update({
+          where: {
+            email: user.email!,
+          },
+          data: {
+            earlyAccess: true,
+          },
+        });
+        await prisma.onboard.update({
+          where: {
+            email: user.email!,
+          },
+          data: {
+            onboarded: true,
+          },
+        });
+      }
+
       const result = await prisma.user.findUnique({
         where: {
           email: user.email!,
